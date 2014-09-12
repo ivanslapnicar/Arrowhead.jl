ecd01c57a4f63fd2c987a0488abb7b5decff2a22
jake bolewski <jakebolewski@gmail.com>
2014-09-12 18:17:59 -0400

    add exports

--- src/Arrowhead.jl
+++ src/Arrowhead.jl
@@ -1,5 +1,8 @@
 module Arrowhead
 using DoubleDouble
+
+export GenSymArrow, SymArrow, aheig, bisect, invAGenSymDPR1,SymDPR1, aheigall
+
 include("arrowhead1.jl")
 include("arrowhead3.jl")
 end # module
