2481d608c614633e8ae350dceb3a459253632b71
Ivan Slapnicar <slapnica@mit.edu>
2014-09-16 21:59:37 +0200

    Update Arrowhead.jl

--- src/Arrowhead.jl
+++ src/Arrowhead.jl
@@ -1,7 +1,7 @@
 module Arrowhead
 using DoubleDouble
 
-export GenSymArrow, SymArrow, aheig, bisect, invAGenSymDPR1,SymDPR1, aheigall
+export GenSymArrow, SymArrow, aheig, bisect, invA, GenSymDPR1,SymDPR1, aheigall
 
 include("arrowhead1.jl")
 include("arrowhead3.jl")
